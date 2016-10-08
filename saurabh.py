45acd06bd9dec471e1667ffa6cbbb27e62c061a9
sudhir <dhumalpranay619@gmail.com>
2016-10-08 09:31:53 +0530

    added functions

--- saurabh.py
+++ saurabh.py
@@ -1,9 +1,10 @@
 # program to identify the even/odd state of given number
+def isEven(number)
+    if number % 2 == 0:
+        print ("The entered number is even")
+    else:
+                print ("The entered number is odd")
 
 number = int (input ("Enter a number: "))
 
-if number % 2 == 0:
-    print ("The entered number is even")
-else:
-            print ("The entered number is odd")
-            
+isEven(number)            
